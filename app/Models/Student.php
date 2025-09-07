e30e963a0fee74d6e8a14549371ab6cfade78c52
ChanMyaeLwin <20352679+ChanMyaeLwin@users.noreply.github.com>
2025-09-08 04:28:04 +0630

    feat: Create Student model

---
+++ app/Models/Student.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'date_of_birth',
+        'gender',
+        'address',
+        'phone_number',
+        'enrollment_date',
+    ];
+
+    /**
+     * Get the user that owns the student profile.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
